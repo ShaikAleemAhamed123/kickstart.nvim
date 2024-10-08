ae7990de36802c6cdc79e9feeb9b8ce9cef50333
Aleem Ahamed Shaik <shaikaleemahamed786@gmail.com>
2024-10-08 21:31:42 +0530

    Cleaned init.lua, moved Neotree keymap command from init.lua to plugin file, commented lazy-lock in .gitignore.

--- init.lua
+++ init.lua
@@ -954,4 +954,3 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
